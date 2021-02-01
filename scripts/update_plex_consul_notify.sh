4ffd7832b9e738f82925162d5574ea25d4331e10
Brent Holden <brent.holden@gmail.com>
2021-02-01 14:46:51 -0500

    Script safety from website errors

--- scripts/update_plex_consul_notify.sh
+++ scripts/update_plex_consul_notify.sh
@@ -5,9 +5,12 @@ source ${BASH_SOURCE%/*}/variables.sh
 #PLEX_VER_IN_USE=`curl -s -H "Content-type: application/json" -H "Accept: application/json" -H "${PLEX_TOKEN}" http://plex.service.consul:32400/ | jq -r '.MediaContainer.version'`
 PLEX_VER_ONSITE=`curl -s -H "${PLEX_TOKEN}" "https://plex.tv/api/downloads/5.json?channel=plexpass" | jq -r '.computer.Linux.version'`
 
-#echo "Found version on system: ${PLEX_VER_IN_USE}"
-
-echo "Found version on plex.tv: ${PLEX_VER_ONSITE}"
-
-consul kv put plex/config/version ${PLEX_VER_ONSITE}
+if [ $? -eq 0 ] && [ ${PLEX_VER_ONSITE} != null ]
+then
+  #echo "Found version on system: ${PLEX_VER_IN_USE}"
+  echo "Found version on plex.tv: ${PLEX_VER_ONSITE}"
+  consul kv put plex/config/version ${PLEX_VER_ONSITE}
+else
+  echo "Caught error trying to fetch version from Plex.tv: Error $?"
+fi
 
