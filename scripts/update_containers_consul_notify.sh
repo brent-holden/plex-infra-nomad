a3faa48efb93eb840120183bbeb48c2c75dcd2df
Brent Holden <brent.holden@gmail.com>
2021-02-01 15:51:51 -0500

    Updated to also write container release to KV

--- scripts/update_containers_consul_notify.sh
+++ scripts/update_containers_consul_notify.sh
@@ -4,7 +4,9 @@ source ${BASH_SOURCE%/*}/variables.sh
 
 for SERVICE in "${!SERVICES[@]}"
 do
-  IMAGE=${SERVICES[${SERVICE}]}
+  IMGANDREPO=${SERVICES[${SERVICE}]}
+  IMAGE=`echo ${IMGANDREPO} | awk -F : '{ print $1 }'`
+  RELEASE=`echo ${IMGANDREPO} | awk -F : '{ print $2 }'`
 
   # pull new image
   sudo podman pull ${IMAGE}
@@ -14,6 +16,9 @@ do
  
   # echo "Writing: ${SERVICE}/config/image as ${IMAGE}"
   consul kv put ${SERVICE}/config/image ${IMAGE}
+
+  # echo "Writing: ${SERVICE}/config/release as ${RELEASE}"
+  consul kv put ${SERVICE}/config/release ${RELEASE}
 
   # echo "Writing: ${SERVICE}/config/image_id as $ID"
   consul kv put ${SERVICE}/config/image_id $ID
