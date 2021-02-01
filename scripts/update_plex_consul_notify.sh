01883435fcaa24c0139802bd8a06fd8fd48fabb9
Brent Holden <brent.holden@gmail.com>
2021-02-01 14:38:36 -0500

    Updated plex output

--- scripts/update_plex_consul_notify.sh
+++ scripts/update_plex_consul_notify.sh
@@ -2,11 +2,12 @@
 
 source ${BASH_SOURCE%/*}/variables.sh
 
-#PLEX_VER_IN_USE=`curl -s -H "Content-type: application/json" -H "Accept: application/json" -H "${PLEX_TOKEN}" http://localhost:32400/ | jq -r '.MediaContainer.version'`
+#PLEX_VER_IN_USE=`curl -s -H "Content-type: application/json" -H "Accept: application/json" -H "${PLEX_TOKEN}" http://plex.service.consul:32400/ | jq -r '.MediaContainer.version'`
 PLEX_VER_ONSITE=`curl -s -H "${PLEX_TOKEN}" "https://plex.tv/api/downloads/5.json?channel=plexpass" | jq -r '.computer.Linux.version'`
 
 #echo "Found version on system: ${PLEX_VER_IN_USE}"
-#echo "Found version on site: ${PLEX_VER_ONSITE}"
+
+echo "Found version on plex.tv: ${PLEX_VER_ONSITE}"
 
 consul kv put plex/config/version ${PLEX_VER_ONSITE}
 
