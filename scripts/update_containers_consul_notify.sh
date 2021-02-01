68205658fc499cdf99fbc436efc6228e4e59078c
Brent Holden <brent.holden@gmail.com>
2021-01-31 22:11:16 -0500

    Comment out some echos

--- scripts/update_containers_consul_notify.sh
+++ scripts/update_containers_consul_notify.sh
@@ -11,9 +11,9 @@ do
   # get consul kv key
   KEY=`echo $IMAGE | awk -F '/' '{print $3}' | awk -F ':' '{print $1}'`
 
-  echo "Writing: $KEY/config/image as $IMAGE"
+  # echo "Writing: $KEY/config/image as $IMAGE"
   consul kv put $KEY/config/image $IMAGE
 
-  echo "Writing: $KEY/config/image_id as $ID"
+  # echo "Writing: $KEY/config/image_id as $ID"
   consul kv put $KEY/config/image_id $ID
 done
