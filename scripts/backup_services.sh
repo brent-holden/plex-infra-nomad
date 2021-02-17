20af7d08673d580dd2d909e52ae06f84b661512b
Brent Holden <brent.holden@gmail.com>
2021-02-17 14:54:44 -0500

    Updated for readability

--- scripts/backup_services.sh
+++ scripts/backup_services.sh
@@ -18,25 +18,25 @@ for SERVICE in "${!SERVICES[@]}"; do
   # Define variables per service
   FILENAME=backup_${DATE}.tar.gz
   LATEST=backup_latest.tar.gz
-  SRCDIR=${BACKUPS[${SERVICE}]}
-  DESTDIR=${RCLONE_BACKUP_DIR}/${SERVICE}
+  SRC_DIR=${BACKUPS[${SERVICE}]}
+  DEST_DIR=${RCLONE_BACKUP_DIR}/${SERVICE}
 
   # Change into service backups directory
-  cd ${SRCDIR}
+  cd ${SRC_DIR}
 
   MATCHING=`ls *.zip 2>/dev/null | wc -l`
   if [ ${MATCHING} -eq "0" ]; then
     # Create the backup file
-    echo "Backing up ${SRCDIR} to ${TMP_DIR}/${FILENAME}"
+    echo "Backing up ${SRC_DIR} to ${TMP_DIR}/${FILENAME}"
     sudo tar -cpzf ${TMP_DIR}/${FILENAME} . 2>/dev/null
 
     # Move it to the right place
-    echo "Moving ${FILENAME} to ${DESTDIR}"
-    sudo mv ${TMP_DIR}/${FILENAME} ${DESTDIR}
+    echo "Moving ${FILENAME} to ${DEST_DIR}"
+    sudo mv ${TMP_DIR}/${FILENAME} ${DEST_DIR}
   else
-    echo "Found zip file backups in ${SRCDIR}. No need to create backups"
-    echo "Copying them over to ${DESTDIR}"
-    sudo cp *.zip ${DESTDIR}
+    echo "Found zip file backups in ${SRC_DIR}. No need to create backups"
+    echo "Copying them over to ${DEST_DIR}"
+    sudo cp *.zip ${DEST_DIR}
   fi
 done
 
