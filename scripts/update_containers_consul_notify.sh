0ad26710c8abd8a6e3cb584c3c2f16ebf81c792b
Brent Holden <brent.holden@gmail.com>
2021-02-26 12:07:24 -0500

    Changed to using vfs caching instead of the cache backend which has been deprecated

--- scripts/update_containers_consul_notify.sh
+++ scripts/update_containers_consul_notify.sh
@@ -13,15 +13,20 @@ do
   podman pull ${IMAGE}:${RELEASE}
 
   # get image Id
-  ID=`podman inspect --format "{{.Id}}" ${IMAGE}:${RELEASE}`
+  ID=$(podman inspect --format "{{.Id}}" ${IMAGE}:${RELEASE})
+  DIGEST=$(podman inspect --format "{{.Digest}}" ${IMAGE}:${RELEASE})
 
   # Now write all the values to Consul KV
-  # echo "Writing: ${SERVICE}/config/image as ${IMAGE}"
+#  echo "Writing: ${SERVICE}/config/image as ${IMAGE}"
   consul kv put ${SERVICE}/config/image ${IMAGE}
 
-  # echo "Writing: ${SERVICE}/config/release as ${RELEASE}"
+#  echo "Writing: ${SERVICE}/config/release as ${RELEASE}"
   consul kv put ${SERVICE}/config/release ${RELEASE}
 
-  # echo "Writing: ${SERVICE}/config/image_id as ${ID}"
+#  echo "Writing: ${SERVICE}/config/image_id as ${ID}"
   consul kv put ${SERVICE}/config/image_id ${ID}
+
+#  echo "Writing: ${SERVICE}/config/image_digest as ${DIGEST}"
+  consul kv put ${SERVICE}/config/image_digest ${DIGEST}
+
 done
