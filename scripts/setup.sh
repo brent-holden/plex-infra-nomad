c4cc2e16c39bc81e757cf134e42f783cf41a76a8
Brent Holden <brent.holden@gmail.com>
2021-03-01 00:43:41 -0500

    Added start_services.sh to media host

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -19,6 +19,7 @@ function setup_media_host() {
   source ${BASH_SOURCE%/*}/setup_backup.sh
   source ${BASH_SOURCE%/*}/update_containers_consul_notify.sh
   source ${BASH_SOURCE%/*}/update_plex_consul_notify.sh
+  source ${BASH_SOURCE%/*}/start_services.sh
 }
 
 function usage() {
