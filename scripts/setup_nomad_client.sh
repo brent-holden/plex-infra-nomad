408d786328d3b193ff9324821742c7cb42372687
Brent Holden <brent.holden@gmail.com>
2022-01-26 13:40:45 -0500

    Added devel_kubic_libcontainers_stable repo to grab containernetworking-plugins as an RPM

--- scripts/setup_nomad_client.sh
+++ scripts/setup_nomad_client.sh
@@ -8,10 +8,18 @@ source ${BASH_SOURCE%/*}/variables.sh
 echo -e "\n\n### Setting up Nomad Client ###\n\n"
 
 echo "Configuring repos and installing packages"
-yum install -y yum-utils git golang make
+yum install -y yum-utils git golang make curl
 
+# Install repositories
 yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/CentOS_8/devel:kubic:libcontainers:stable.repo
+
 yum install -y nomad
+yum install -y containernetworking-plugins
+
+# Link the CNI binaries directory so that Nomad works with Consul Connect
+mkdir ${OPT_DIR}/cni
+ln -sf /usr/libexec/cni ${OPT_DIR}/cni/bin
 
 cp ${BASH_SOURCE%/*}/../config/nomad/client.hcl /etc/nomad.d/nomad.hcl
 
