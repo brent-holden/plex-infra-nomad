7e4dfee601bfd3767af446219703af21aa601e4a
Brent Holden <brent.holden@gmail.com>
2022-01-26 12:39:23 -0500

    Added OPT_DIR variable to location of backups array

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -1,33 +1,5 @@
 #!/usr/bin/env bash
 
-declare -A SERVICES=( [lidarr]=lscr.io/linuxserver/lidarr:latest,auto_update
-                      [sonarr]=lscr.io/linuxserver/sonarr:latest,auto_update
-                      [radarr]=lscr.io/linuxserver/radarr:latest,auto_update
-                      [readarr]=lscr.io/linuxserver/readarr:nightly,auto_update
-                      [prowlarr]=lscr.io/linuxserver/prowlarr:nightly,auto_update
-                      [kavita]=docker.io/kizaing/kavita:latest:latest,auto_update
-                      [ubooquity]=lscr.io/linuxserver/ubooquity:latest:latest,auto_update
-                      [tautulli]=lscr.io/linuxserver/tautulli:latest,auto_update
-                      [sabnzbd]=lscr.io/linuxserver/sabnzbd:latest,auto_update
-                      [ombi]=lscr.io/linuxserver/ombi:latest,auto_update
-                      [caddy]=docker.io/library/caddy:alpine,auto_update
-                      [traefik]=docker.io/library/traefik:latest,auto_update
-                      [plex]=docker.io/plexinc/pms-docker:plexpass,auto_update
-                    )
-declare -A BACKUPS=(  [lidarr]=/opt/lidarr/Backups/scheduled
-                      [sonarr]=/opt/sonarr/Backups/scheduled
-                      [radarr]=/opt/radarr/Backups/scheduled
-                      [readarr]=/opt/readarr/Backups/scheduled
-                      [prowlarr]=/opt/prowlarr/Backups/scheduled
-                      [kavita]=/opt/kavita/
-                      [ubooquity]=/opt/ubooquity/
-                      [tautulli]=/opt/tautulli/backups
-                      [sabnzbd]=/opt/sabnzbd/
-                      [ombi]=/opt/ombi/
-                      [caddy]=/opt/caddy/
-                      [traefik]=/opt/traefik/config/
-                      [plex]=/opt/plex/Backups
-                    )
 DOWNLOADABLES=(movies tv music other)
 DATE=`date +%d-%m-%Y`
 CRON_DIR=/etc/cron.d
@@ -54,5 +26,31 @@ SYSTEMD_DIR=/usr/lib/systemd/system
 PLEX_USER=plex
 PLEX_GROUP=plex
 PLEX_UID=1100
-PACKAGES="fuse rsync vim git podman"
-NOMAD_PLUGIN_DIR=/opt/nomad/plugins
+PACKAGES="fuse rsync vim git"
+
+declare -A SERVICES=( [caddy]=docker.io/library/caddy:alpine,auto_update
+                      [kavita]=docker.io/kizaing/kavita:latest:latest,auto_update
+                      [lidarr]=lscr.io/linuxserver/lidarr:latest,auto_update
+                      [ombi]=lscr.io/linuxserver/ombi:latest,auto_update
+                      [plex]=docker.io/plexinc/pms-docker:plexpass,auto_update
+                      [prowlarr]=lscr.io/linuxserver/prowlarr:nightly,auto_update
+                      [radarr]=lscr.io/linuxserver/radarr:latest,auto_update
+                      [readarr]=lscr.io/linuxserver/readarr:nightly,auto_update
+                      [sabnzbd]=lscr.io/linuxserver/sabnzbd:latest,auto_update
+                      [sonarr]=lscr.io/linuxserver/sonarr:latest,auto_update
+                      [tautulli]=lscr.io/linuxserver/tautulli:latest,auto_update
+                      [traefik]=docker.io/library/traefik:latest,auto_update
+                    )
+declare -A BACKUPS=(  [caddy]=${OPT_DIR}/caddy/
+                      [kavita]=${OPT_DIR}/kavita/
+                      [lidarr]=${OPT_DIR}/lidarr/Backups/scheduled
+                      [ombi]=${OPT_DIR}/ombi/
+                      [plex]=${OPT_DIR}/plex/Backups
+                      [prowlarr]=${OPT_DIR}/prowlarr/Backups/scheduled
+                      [radarr]=${OPT_DIR}/radarr/Backups/scheduled
+                      [readarr]=${OPT_DIR}/readarr/Backups/scheduled
+                      [sabnzbd]=${OPT_DIR}/sabnzbd/
+                      [sonarr]=${OPT_DIR}/sonarr/Backups/scheduled
+                      [tautulli]=${OPT_DIR}/tautulli/backups
+                      [traefik]=${OPT_DIR}/traefik/config/
+                    )
