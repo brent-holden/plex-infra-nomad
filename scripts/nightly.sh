82dde7730c7a3a7c81fdeac5f028ca74a383efca
Brent Holden <brent.holden@gmail.com>
2023-04-04 10:27:46 -0400

    Removed updater from nightly

--- scripts/nightly.sh
+++ scripts/nightly.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-# Pull new containers and update Consul key
-source ${BASH_SOURCE%/*}/update_containers_consul_notify.sh
-
-# Update Consul key with Plexpass version
-source ${BASH_SOURCE%/*}/update_plex_consul_notify.sh
-
 # Remove stale downloads
 source ${BASH_SOURCE%/*}/prune_downloads.sh
 
-# Remove stale containers
-source ${BASH_SOURCE%/*}/clean_docker.sh
