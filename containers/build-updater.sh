535ea45770e62d73cd76d5965c392f993acdec5b
Brent Holden <brent.holden@gmail.com>
2023-04-04 12:47:31 -0400

    Updated to remove sudo

--- containers/build-updater.sh
+++ containers/build-updater.sh
@@ -6,8 +6,8 @@ if [ ! -n "$1" ]; then
 fi
 
 cd update-services
-sudo docker build -t bholden/update-services .
-sudo docker tag bholden/update-services:latest bholden/update-services:$1
-sudo docker push bholden/update-services:$1
-sudo docker push bholden/update-services:latest
+docker build -t bholden/update-services .
+docker tag bholden/update-services:latest bholden/update-services:$1
+docker push bholden/update-services:$1
+docker push bholden/update-services:latest
 
