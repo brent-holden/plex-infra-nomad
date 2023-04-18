6d2d18dcee98b20cd76c3d6dbe67e6d8ff7d27f4
Brent Holden <brent.holden@gmail.com>
2023-04-17 23:43:31 -0400

    Removed services hard coding

--- containers/update-services/update-services.py
+++ containers/update-services/update-services.py
@@ -5,22 +5,6 @@ import yaml
 import requests
 import sys
 
-services = [
-            "authelia",
-            "kavita",
-            "lidarr",
-            "netbootxyz",
-            "overseerr",
-            "prowlarr",
-            "radarr",
-            "rclone_restic",
-            "readarr",
-            "sabnzbd",
-            "sonarr",
-            "tautulli",
-            "traefik",
-            ]
-
 try:
     with open('levant.yml', 'r') as file:
         levant = yaml.safe_load(file)
@@ -34,28 +18,31 @@ except:
     print("Couldn't open connection to Consul. Exiting", file=sys.stderr)
     sys.exit(1)
 
+
+services = levant['app']
+
 for service in services:
     index, values = consul_handler.kv.get("%s/config/auto_update" % service)
 
-    try:
-        digest_url = levant['app'][service]['container']['check_digest_url']
-        output = requests.get(digest_url).json()
+    if values['Value'].decode('utf8') == 'True' and service != 'plex':
+        try:
+            digest_url = levant['app'][service]['container']['check_digest_url']
+            output = requests.get(digest_url).json()
 
-        if values['Value'].decode('utf8') == 'true':
-            print("Putting digest " + output['digest'] + " on key " + "%s/config/image_digest" % service)
-            consul_handler.kv.put("%s/config/image_digest" % service, output['digest'])
-        else:
-            print("Skipped %s because auto_update was set to false" % service)
+            images = output['images']
+            for image in images:
+                if image['architecture'] == services[service]['container']['architecture']:
+                    print("Putting digest " + image['digest'] + " on key " + "%s/config/image_digest" % service)
+                    consul_handler.kv.put("%s/config/image_digest" % service, image['digest'])
 
-    except TypeError:
-            print("No value for auto_update for %s" % service)
+        except IndexError:
+                print("No value for auto_update for %s" % service)
 
-    except:
-        print("Something broke trying to get the JSON data from the URL. BUSTED!")
-
+    else:
+        print("Skipped updating %s" % service)
 try:
     index, values = consul_handler.kv.get("plex/config/auto_update")
-    if values['Value'].decode('utf8') == 'true':
+    if values['Value'].decode('utf8') == 'True':
 
         plex_digest_url = levant['app']['plex']['container']['check_digest_url']
         plex_output = requests.get(plex_digest_url).json()
