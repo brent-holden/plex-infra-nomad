07dbe5bb01f29c71fc1dba0928c124378d42c9bd
Brent Holden <brent.holden@gmail.com>
2023-04-29 16:25:53 -0400

    Updated to exec init services locally

--- scripts/init_services.py
+++ scripts/init_services.py
@@ -6,12 +6,19 @@ import requests
 import sys
 import os
 
-
-if len(sys.argv) < 2:
-    print("Usage: python3 program.py <config_file>")
+if len(sys.argv) < 3:
+    print("Usage: python3 program.py -f <levant_file> -c <consul_server>")
     sys.exit(1)
 
-config_file = sys.argv[1]
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--config_file", help="path to configuration file")
+parser.add_argument("-c", "--consul_host", help="Consul host name or IP address")
+args = parser.parse_args()
+
+config_file = args.config_file
+consul_host = args.consul_host
 
 try:
     with open(config_file, "r") as file:
@@ -21,7 +28,7 @@ except FileNotFoundError:
     sys.exit(1)
 
 try:
-    consul_handler = consul.Consul()
+    consul_handler = consul.Consul(host=consul_host)
 except:
     print("Couldn't open connection to Consul. Exiting", file=sys.stderr)
     sys.exit(1)
