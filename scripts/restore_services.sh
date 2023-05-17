9e2963c1172ae7c5d6c13d01375b5771439bbf93
Brent Holden <brent.holden@gmail.com>
2023-05-17 16:08:03 -0400

    Updated

--- scripts/restore_services.sh
+++ scripts/restore_services.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker run --rm -e RESTIC_PASSWORD=phalanx -v /opt/services/lidarr:/config restic/restic --repo rest:http://restic-endpoint.service.consul:7070/lidarr restore latest --target /
-docker run --rm -e RESTIC_PASSWORD=phalanx -v /opt/services/radarr:/config restic/restic --repo rest:http://restic-endpoint.service.consul:7070/radarr restore latest --target /
-docker run --rm -e RESTIC_PASSWORD=phalanx -v /opt/services/sonarr:/config restic/restic --repo rest:http://restic-endpoint.service.consul:7070/sonarr restore latest --target /
-docker run --rm -e RESTIC_PASSWORD=phalanx -v /opt/services/readarr:/config restic/restic --repo rest:http://restic-endpoint.service.consul:7070/readarr restore latest --target /
-docker run --rm -e RESTIC_PASSWORD=phalanx -v /opt/services/prowlarr:/config restic/restic --repo rest:http://restic-endpoint.service.consul:7070/prowlarr restore latest --target /
-docker run --rm -e RESTIC_PASSWORD=phalanx -v /opt/services/overseerr:/config restic/restic --repo rest:http://restic-endpoint.service.consul:7070/overseerr restore latest --target /
-docker run --rm -e RESTIC_PASSWORD=phalanx -v /opt/services/sabnzbd:/config restic/restic --repo rest:http://restic-endpoint.service.consul:7070/sabnzbd restore latest --target /
-docker run --rm -e RESTIC_PASSWORD=phalanx -v /opt/services/kavita:/config restic/restic --repo rest:http://restic-endpoint.service.consul:7070/kavita restore latest --target /
+docker run --rm -e RESTIC_PASSWORD=$RESTIC_PASSWORD -v /opt/services/lidarr:/config restic/restic --repo rest:http://restic-endpoint.service.consul:7070/lidarr restore latest --target /
+docker run --rm -e RESTIC_PASSWORD=$RESTIC_PASSWORD -v /opt/services/radarr:/config restic/restic --repo rest:http://restic-endpoint.service.consul:7070/radarr restore latest --target /
+docker run --rm -e RESTIC_PASSWORD=$RESTIC_PASSWORD -v /opt/services/sonarr:/config restic/restic --repo rest:http://restic-endpoint.service.consul:7070/sonarr restore latest --target /
+docker run --rm -e RESTIC_PASSWORD=$RESTIC_PASSWORD -v /opt/services/readarr:/config restic/restic --repo rest:http://restic-endpoint.service.consul:7070/readarr restore latest --target /
+docker run --rm -e RESTIC_PASSWORD=$RESTIC_PASSWORD -v /opt/services/prowlarr:/config restic/restic --repo rest:http://restic-endpoint.service.consul:7070/prowlarr restore latest --target /
+docker run --rm -e RESTIC_PASSWORD=$RESTIC_PASSWORD -v /opt/services/overseerr:/config restic/restic --repo rest:http://restic-endpoint.service.consul:7070/overseerr restore latest --target /
+docker run --rm -e RESTIC_PASSWORD=$RESTIC_PASSWORD -v /opt/services/sabnzbd:/config restic/restic --repo rest:http://restic-endpoint.service.consul:7070/sabnzbd restore latest --target /
+docker run --rm -e RESTIC_PASSWORD=$RESTIC_PASSWORD -v /opt/services/kavita:/config restic/restic --repo rest:http://restic-endpoint.service.consul:7070/kavita restore latest --target /
